d2481be53e926f5a3178d875c3b7f6d48c700e56
Henrik Hasell <henrik@henrik.si>
2020-02-16 18:27:53 +0000

    some work

--- web/poem.py
+++ web/poem.py
@@ -8,10 +8,10 @@ from dateutil.relativedelta import relativedelta
 class PoemGenerator:
     def __init__(self):
         now = datetime.now()
-        self.combined_files = self._get_text(datetime(1970, 1, 1), now)
-        self.combined_this_year = self._get_text(now - relativedelta(years=1), now)
-        self.combined_this_month = self._get_text(now - relativedelta(months=1), now)
-        self.combined_today = self._get_text(now - relativedelta(days=1), now)
+        self.combined_files = lambda: self._get_text(datetime(1970, 1, 1), now)
+        self.combined_this_year = lambda: self._get_text(now - relativedelta(years=1), now)
+        self.combined_this_month = lambda: self._get_text(now - relativedelta(months=1), now)
+        self.combined_today = lambda: self._get_text(now - relativedelta(days=1), now)
 
     def _get_this_year_text(self):
         now = datetime.now()
@@ -33,10 +33,12 @@ class PoemGenerator:
             'today': self.combined_today
         }
 
+        chosen_dict = markov_dict[mode]()
+
         result = []
 
         for i in range(4):
-            result += [markov_dict[mode].make_short_sentence(75)]
+            result += [chosen_dict.make_short_sentence(75)]
 
         return [i for i in result if i != None]
 
